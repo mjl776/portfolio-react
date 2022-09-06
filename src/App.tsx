21e44a039c9c4513aac07b44cf515741e3d1f6c8
Michael Lee <mlee@10-16-13-187.dynapool.wireless.nyu.edu>
2022-09-06 11:59:51 -0400

    new redesign of mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import './App.css';
 import NavBar from './NavigationBar/NavBar'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './HomePage/Homepage';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/esm/Row';
-import Col from 'react-bootstrap/esm/Col';
+
 const App = () => {
   return (
     <Router>
@@ -14,7 +12,6 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomePage/>}></Route>
           </Routes>
- 
       </div>
       
     </Router>
