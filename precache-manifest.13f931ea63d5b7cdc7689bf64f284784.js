69be27d7a30d34c3a3190f4cfa0927791b864f5e
Michael Lee <mlee@10-16-28-206.dynapool.wireless.nyu.edu>
2022-09-19 12:01:00 -0400

    Updates

---
+++ precache-manifest.13f931ea63d5b7cdc7689bf64f284784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8006c31da21651ba4cb4",
+    "url": "/static/css/main.b59f51e1.chunk.css"
+  },
+  {
+    "revision": "8006c31da21651ba4cb4",
+    "url": "/static/js/main.8006c31d.chunk.js"
+  },
+  {
+    "revision": "17803a3b2760261e7246",
+    "url": "/static/js/1.17803a3b.chunk.js"
+  },
+  {
+    "revision": "800fa476ae0f28b39bfa",
+    "url": "/static/js/2.800fa476.chunk.js"
+  },
+  {
+    "revision": "2a12359ab8d2394c837c",
+    "url": "/static/js/runtime~main.2a12359a.js"
+  },
+  {
+    "revision": "898a9170c2ade7622a29d200c26506fd",
+    "url": "/static/media/SierraProjectPhoto.898a9170.png"
+  },
+  {
+    "revision": "2e79edd7162de35b960dea702ec61a29",
+    "url": "/index.html"
+  }
+];
