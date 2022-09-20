f734eb34d4bdb660965825ac88df07bf6ba51419
Michael Lee <mlee@10-16-28-206.dynapool.wireless.nyu.edu>
2022-09-20 12:56:33 -0400

    Updates

--- precache-manifest.c639f12834d4ef5e91ffa8293f8d0894.js
+++ precache-manifest.c639f12834d4ef5e91ffa8293f8d0894.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "3b04c61bebdf8464365b",
+    "revision": "317cc05bbc1e5b260e4e",
     "url": "/static/css/main.b59f51e1.chunk.css"
   },
   {
-    "revision": "3b04c61bebdf8464365b",
-    "url": "/static/js/main.3b04c61b.chunk.js"
+    "revision": "317cc05bbc1e5b260e4e",
+    "url": "/static/js/main.317cc05b.chunk.js"
   },
   {
     "revision": "17803a3b2760261e7246",
     "url": "/static/js/1.17803a3b.chunk.js"
   },
   {
-    "revision": "80a6ba2590c2b8f83472",
-    "url": "/static/js/2.80a6ba25.chunk.js"
+    "revision": "6114714e24855bc195ae",
+    "url": "/static/js/2.6114714e.chunk.js"
   },
   {
     "revision": "2a12359ab8d2394c837c",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/static/media/SierraProjectPhoto.898a9170.png"
   },
   {
-    "revision": "146569fe1ee8dd1ffb2e530a2444680b",
+    "revision": "5be9d01bfd227cc9f4f8f78a0d0cdde8",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1246ec014420276d30488d69b24ed49.js"
+  "/precache-manifest.c639f12834d4ef5e91ffa8293f8d0894.js"
 );
 
 workbox.clientsClaim();
